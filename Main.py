31fcd23810671243f88aee26651c2a70f1e94ab8
Conn O'Flynn <connoflynn@gmail.com>
2020-12-09 21:22:34 +0000

    Added writing results to file functionality

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
+import datetime
 from Data import get_data, split_data, remove_beerid, get_class_count, split_for_branches
 from Entropy import get_entropy
 from InformationGain import get_information_gain, find_split_indexes, choose_best_attribute, predict_attributes
 from Tree import create_tree
 
 def main():
+    begin_time = datetime.datetime.now()
 
     test_results = dict()
-    
-
+    all_incorrect_guesses = list() # will be a list of lists containing the incorrect guesses for each iteration
     #perform the algorithm test
     for i in range(0,10):
         data = get_data("beer.csv")
@@ -37,11 +38,12 @@ def main():
                 incorrect_guesses.append((index, predicted_attribute , actual_attribute))
             
         accuracy_percent = '{0:.2f}'.format((float(correct_predictions) / float(num_rows)) * 100)
-        test_results[i+1] = accuracy_percent
+        test_results[i+1] = accuracy_percent # add accuracy to test_results
         print("\nAccuracy Percent: ", accuracy_percent, "%")
         print("\nIncorrect Guesses:")
         for index, predicted, actual in incorrect_guesses:
             print("Index: ", index, "\tPredicted: ", predicted, "\tActual: ", actual)
+        all_incorrect_guesses.append(incorrect_guesses)
         print("------------------------")
     
     accuracy_average = 0.0
@@ -50,5 +52,23 @@ def main():
     
     accuracy_average = accuracy_average / len(test_results)
     print("Average accurate percentage: ", accuracy_average, "%")
+    
+    print("Writing results to file.") 
+    # write a summary of the results to a csv file
+    with open("test_results.txt",'w') as f:
+        for result in test_results.items():
+            f.write("Test: %s" % str(result[0]))
+            f.write("\nAccuracy: %s" % str(result[1]))
+            f.write("\nIncorrect Guesses:")
+            for i in range(0,len(all_incorrect_guesses[result[0]-1])):
+                f.write("\nIndex: %s" % str(all_incorrect_guesses[result[0]-1][i][0]))
+                f.write("\tPredicted: %s" % str(all_incorrect_guesses[result[0]-1][i][1]))
+                f.write("\tActual: %s" % str(all_incorrect_guesses[result[0]-1][i][2]))
+            f.write("\n\n")
+        f.close()
+    
+    endtime = datetime.datetime.now()
+    time_taken = endtime - begin_time
+    print("Time taken to run: ", time_taken)
 
 main()
